88606c4e808cc4bd0101095557e8cbdb5b3017f4
chenlili <375587295@qq.com>
2018-03-06 17:16:22 +0800

    add webpack test libaray karma

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @desc webpack打包入口文件
+ */
+
+ const arrayEqual = require('./array/arrayEqual')
+
+ module.exports = {
+	 arrayEqual,
+ }
---
+++ test/arrary.test.js
@@ -0,0 +1,14 @@
+describe("Array API:", function() {
+	describe("#arrayEqual()", function() {
+		it(`gtool.arrayEqual([0, 2, 3], [1, 2, 3]) should return false)`, function() {
+			assert.notEqual(gtool.arrayEqual([0, 2, 3], [1, 2, 3]));
+		});
+		it(`gtool.arrayEqual([1,2,3],[1,2,3,4]) should return true)`, function() {
+			assert(gtool.arrayEqual([1, 2, 3], [1, 2, 3, 4]));
+		});
+		let arr = [9, 2, 3, 4, 5];
+		it(`gtool.arrayEqual([${arr},${arr}]) sholud return true`, function() {
+			assert(gtool.arrayEqual(arr, arr));
+		});
+	});
+});
