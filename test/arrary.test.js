78e7a9e589563239ff6bbc6a74d26c75af617621
chenlili <375587295@qq.com>
2018-03-07 10:19:54 +0800

    travis.yml

--- test/arrary.test.js
+++ test/arrary.test.js
@@ -3,8 +3,8 @@ describe("Array API:", function() {
 		it(`gtool.arrayEqual([0, 2, 3], [1, 2, 3]) should return false)`, function() {
 			assert.notEqual(gtool.arrayEqual([0, 2, 3], [1, 2, 3]));
 		});
-		it(`gtool.arrayEqual([1,2,3],[1,2,3,4]) should return true)`, function() {
-			assert(gtool.arrayEqual([1, 2, 3], [1, 2, 3, 4]));
+		it(`gtool.arrayEqual([1,2,3],[1,2,3]) should return true)`, function() {
+			assert(gtool.arrayEqual([1, 2, 3], [1, 2 ,3]));
 		});
 		let arr = [9, 2, 3, 4, 5];
 		it(`gtool.arrayEqual([${arr},${arr}]) sholud return true`, function() {
